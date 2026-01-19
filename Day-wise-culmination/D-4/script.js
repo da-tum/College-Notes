9dffc497998748f621a83639f560dddfd2fab046
Inkesk-Dozing <inkeskdozing@gmail.com>
2026-01-19 15:01:56 +0530

    In class Q2

--- Day-wise-culmination/D-4/script.js
+++ Day-wise-culmination/D-4/script.js
@@ -1,5 +1,27 @@
-let arr=[1,2,3,4,5];
-while (arr.length>0){
-    arr.push(arr.pop());
-}
-console.log(arr);
+// A1
+// let arr=[1,2,3,4,5];
+// let revArr=[];
+// while (arr.length>0){
+//     revArr.push(arr.pop());
+// }
+// console.log(revArr);
+
+// A2
+// let arr= [-1,-2,3,4,-5,6];
+// let posArr=[];
+// while (arr.length>0){
+//     let val=arr.pop();
+//     if (val>0){
+//         posArr.push(val);
+//     }   
+// }
+// console.log(posArr);
+
+// or 
+
+// for (let i=arr.length-1;i>=0;i--){
+//     if (arr[i]>0){
+//         posArr.push(arr[i]);
+//     }
+// }
+// console.log(posArr);
