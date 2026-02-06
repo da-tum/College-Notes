b958a67c1818bedfed57d053bbffc02d4176f146
Inkesk-Dozing <inkeskdozing@gmail.com>
2026-02-06 10:08:58 +0530

    Day10 Added

--- Sem-II/Web-Dev-II/Day-wise-culmination/D-10/script.js
+++ Sem-II/Web-Dev-II/Day-wise-culmination/D-10/script.js
@@ -1,19 +1,16 @@
-// Wait for the DOM to load before executing
-document.addEventListener('DOMContentLoaded', function() {
-    // Create new element and set its content
-    const newParagraph = document.createElement('p');
-    newParagraph.textContent = 'This is a dynamically added paragraph.';
-    document.body.appendChild(newParagraph);
+//Create new elemment and set its content
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'This is a dynamically added paragraph.';
+document.body.appendChild(newParagraph);
 
-    // Append the new element in the container
-    const container = document.getElementById('container');
-    container.appendChild(newParagraph);
+// Append the new element in the container 
+const container = document.getElementById('container');
+container.appendChild(newParagraph);
 
-    // Remove the content of the container
-    document.querySelector('#container p').remove();
+// Remove the content of the container
+document.querySelector('#container p').remove();
 
-    const image = document.createElement("img");
-    image.setAttribute("src", "https://picsum.photos/300/200");
-    image.setAttribute("alt", "Placeholder Image");
-    document.body.appendChild(image);
-});
+const image = document.createElement("img");
+image.setAttribute("src", "https://picsum.photos/300/200");
+image.setAttribute("alt", "Placeholder Image");
+document.body.appendChild(image);
