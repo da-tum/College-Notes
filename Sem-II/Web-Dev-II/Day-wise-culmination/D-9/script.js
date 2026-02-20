676346b47171e34e7d018ed9faf7ed819b4d1627
Inkesk-Dozing <inkeskdozing@gmail.com>
2026-02-20 12:00:00 +0000

    Day10 Added
    Squashed 1 minor/update commits.

---
+++ Sem-II/Web-Dev-II/Day-wise-culmination/D-9/script.js
@@ -0,0 +1,16 @@
+//Create new elemment and set its content
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'This is a dynamically added paragraph.';
+document.body.appendChild(newParagraph);
+
+// Append the new element in the container 
+const container = document.getElementById('container');
+container.appendChild(newParagraph);
+
+// Remove the content of the container
+document.querySelector('#container p').remove();
+
+const image = document.createElement("img");
+image.setAttribute("src", "https://picsum.photos/300/200");
+image.setAttribute("alt", "Placeholder Image");
+document.body.appendChild(image);
